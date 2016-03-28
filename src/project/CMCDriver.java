864e13c618a27b0c5e6d10331deeebd857d0e0ae
aonneji <aonneji@csbsju.edu>
2016-03-28 16:33:29 -0500

    Edited cmcdriver

--- src/project/CMCDriver.java
+++ src/project/CMCDriver.java
@@ -15,7 +15,7 @@ public class CMCDriver {
 			
 			     StudentUI studentUI= new StudentUI(args[0],args[1], args[2]);
 			      AdminUI   adminUI = new AdminUI(args[0],args[1], args[2]);
-			   studentUI.logOn("luser", "user");
+			   studentUI.logOn("ash5star", "ash1234");
 			   studentUI.getStudentInfo();
 				 studentUI.getSavedUniversity();
 				 studentUI.saveUni("BOSTON COLLEGE");
@@ -36,7 +36,6 @@ public class CMCDriver {
 					     University tech = new University("Kanye West TECHNOLOGY ","Kanye City","BIG-CITY","PRIVATE",5000,50.0,800,800,800,22000.0,50,1000.5,25.0,70,4,4,"4");
 						 adminUI.addUniversity(tech);
 					     adminUI.logOff();
-				
 			
 		     
 	
