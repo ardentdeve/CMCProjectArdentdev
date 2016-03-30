95f279519b67e820a36b983ff8b7e5b3e7d4bedb
jshinnenkamp <jshinnenkamp@lin23.cs.csbsju.edu>
2016-03-29 21:02:23 -0500

    JavaDoc for Admin

--- src/project/Admin.java
+++ src/project/Admin.java
@@ -3,22 +3,57 @@ package project;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
+/**
+ * Class representing an Admin that is logged into to the system.
+ * 
+ * @author Ardentdeve
+ * @version March, 2016
+ */
+
 public class Admin extends User {
+	
+	/**
+	 * Constructor that creates the Admin
+	 * 
+	 * @param pw
+	 * @param un
+	 * @param firstName
+	 * @param lastName
+	 * @param type
+	 * @param status
+	 */
 
 	public Admin(String pw, String un, String firstName, String lastName, char type, char status) {
 		super(pw, un, firstName, lastName, type, status);
 	}
+	
+	/**
+	 * Constructor that calls the User class with no parameters
+	 */
 	
 	public Admin()
 	{
 		super();
 		
 	}
-	//I love this the most
+	/**
+	 * Constructor that calls the User class with Parameters filled out.
+	 * @param firstName
+	 * @param lastName
+	 * @param username
+	 * @param password
+	 * @param type
+	 */
 	public Admin(String firstName, String lastName, String username, String password, char type) {
 		super(firstName,lastName,username,password,type);
 		
 	}
+	
+	/**
+	 * Returns a String of the Admin information
+	 * 
+	 * @return firstname, lastname, username, password, type, and status of the Admin.
+	 */
 	
 	@Override
 	public String toString() {
