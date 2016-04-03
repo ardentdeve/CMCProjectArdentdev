2a61e76d3d042df215977828927c6a09696d0f64
aonneji <aonneji@csbsju.edu>
2016-04-03 18:26:05 -0500

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/ardentdeve/CMCProjectArdentdev.git
    
    Conflicts:
    	src/project/StudentHome.java

--- testers/project/StudentHomeTest.java
+++ testers/project/StudentHomeTest.java
@@ -44,15 +44,60 @@ public class StudentHomeTest {
 	}
 	
 	@Test 
-	public void removeUniversityFailsForEmptySavedList()
+	public void removeUniversityFailsForSavedList()
 	{
 		Student student = new Student("Al","Jacob","AlJ","aj233",'a');
 		sh.addStudent(student);
 	   assertEquals(0, sh.removeUniversity(student,"APPLE TECHNOLOGY"));
 	    sh.deleteStudent(student);
 	}
+	@Test
+   public void editStudentProfilePasses()
+   {	
+	Student student = sh.login("nicoleK", "Ash1234");
+	assertEquals(1, sh.editProfile("Nicole", "Nneji", "Ash1234"));
+	sh.logoff();
+}
+	@Test(expected =IllegalArgumentException.class)
+	public void editProfileFailsForInvalidUserInfo()
+	{
+		Student student = sh.login("nicoleK123", "Ash1234");
+	  sh.editProfile("Nicole", "Nneji", "Ash1234");	
+	}
+@Test
+public void getSavedUniversityPasses()
+{
+	Student student = sh.login("ash5star", "ash1234");
+	sh.saveUniversity(student, "ADELPHI");
+	assertNotNull(sh.getSavedUniversity());
+	sh.removeUniversity(student,"ADELPHI");
+	sh.logoff();
 	
+}
+@Test(expected = IllegalArgumentException.class)
+public void getSavedUniversityFailsForInvalidUser()
+{
+	Student student = sh.login("ni1oleK1", "Ash1234");
+	sh.getSavedUniversity();
+	sh.logoff();
+}
 
-	
-	
+@Test
+public void testisUserStudent()
+{
+	assertEquals(true,sh.isUserStudent("nicoleK"));
+}
+
+@Test
+public void testisUserStudentFails()
+{
+	assertEquals(false,sh.isUserStudent("nicoleK1"));
+}
+
+
+
+
+
+
+
 }
