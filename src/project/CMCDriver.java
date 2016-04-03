69728a8bc1a280012a5181f4e6ef62e0ae96f83c
jshinnenkamp <jshinnenkamp@lin10.cs.csbsju.edu>
2016-04-03 18:11:31 -0500

    admin home test

--- src/project/CMCDriver.java
+++ src/project/CMCDriver.java
@@ -9,48 +9,48 @@ import java.util.Map.Entry;
 public class CMCDriver {
 	private Student s;
 	private String username;
-	
-	
-	 public static void main(String[] args) {
-			
-			     StudentUI studentUI= new StudentUI(args[0],args[1], args[2]);
-			      AdminUI   adminUI = new AdminUI(args[0],args[1], args[2]);
-			      AdminHome adminH = new AdminHome(args[0],args[1], args[2]);
-			   System.out.println(studentUI.logOn("nicoleK", "Ash1234"));
-			   studentUI.getStudentInfo();
-				 studentUI.getSavedUniversity();
-				 studentUI.saveUni("BOSTON COLLEGE");
-				 studentUI.removeUniversity("BOSTON");
-				 studentUI.logOff();
-				Admin admin = adminH.findAdmin("Rahal");
-				System.out.println(admin.getFirstName());
-			     Student s = new Student("Mimi", "Kardashian", "MimiLuv", "mi1234", 'u', 'Y');
-			   adminUI.addStudent(s);
-			    Student editS = new Student("MimiN", "Kardashian", "MimiLuv", "mi1234", 'u', 'Y');
-			  adminUI.editStudent(editS);
-		      adminUI.getStudents();
-			 adminUI.getAdmins();
-			University tech = new University("Kanye West TECHNOLOGY ","Kanye City","BIG-CITY","PRIVATE",5000,50.0,800,800,800,22000.0,50,1000.5,25.0,70,4,4,"4");
-			adminUI.addUniversity(tech);
-			adminUI.logOff();
-			
-		     
-	
 
-				
-				 
-	 
-		
-				 
-				 
-		          
-			
-			
-			
-			
-			
 
-	
+	public static void main(String[] args) {
 
-}
+		StudentUI studentUI= new StudentUI(args[0],args[1], args[2]);
+		AdminUI   adminUI = new AdminUI(args[0],args[1], args[2]);
+		AdminHome adminH = new AdminHome(args[0],args[1], args[2]);
+		System.out.println(studentUI.logOn("nicoleK", "Ash1234"));
+		studentUI.getStudentInfo();
+		studentUI.getSavedUniversity();
+		studentUI.saveUni("BOSTON COLLEGE");
+		studentUI.removeUniversity("BOSTON");
+		studentUI.logOff();
+		Admin admin = adminH.findAdmin("Rahal");
+		System.out.println(admin.getFirstName());
+		Student s = new Student("Mimi", "Kardashian", "MimiLuv", "mi1234", 'u', 'Y');
+		adminUI.addStudent(s);
+		Student editS = new Student("MimiN", "Kardashian", "MimiLuv", "mi1234", 'u', 'Y');
+		adminUI.editStudent(editS);
+		adminUI.getStudents();
+		adminUI.getAdmins();
+		University tech = new University("Kanye West TECHNOLOGY ","Kanye City","BIG-CITY","PRIVATE",5000,50.0,800,800,800,22000.0,50,1000.5,25.0,70,4,4,"4");
+		adminUI.addUniversity(tech);
+		adminUI.logOff();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
 }
--- testers/project/AdminHomeTest.java
+++ testers/project/AdminHomeTest.java
@@ -64,26 +64,48 @@ public class AdminHomeTest {
 	
 	@Test
 	public void testEditStudent(){
-		
+		Student student = new Student("Jacob", "Hinnenkamp", "jacob4", "hello", 'a', 'Y');
+		sh.addStudent(student);
+		student = new Student("Jacob", "Hinnenkamp", "jacob4", "goodbye", 'a', 'Y');
+		assertEquals(1,ah.editStudent(student));
+		sh.deleteStudent(student);
 	}
 	
 	@Test
 	public void testAddAdmin(){
-		
+		Admin admin = new Admin("Jacob", "Hinnenkamp", "jacob4", "hello", 'a', 'Y');
+		assertEquals(1, ah.addAdmin(admin));
+		Admin newAdmin = ah.findAdmin("jacob4");
+		assertEquals(admin.getUsername(), newAdmin.getUsername());
+		ah.deleteAdmin(admin);
 	}
 	
 	@Test
 	public void testEditAdmin(){
-		
+		Admin admin = new Admin("Jacob", "Hinnenkamp", "jacob4", "hello", 'a', 'Y');
+		ah.addAdmin(admin);
+		admin = new Admin("Jacob", "Hinnenkamp", "jacob4", "goodbye", 'a', 'Y');
+		assertEquals(1,ah.editAdmin(admin));
+		ah.deleteAdmin(admin);
 	}
 	
 	@Test
 	public void testIsUserAdmin(){
-		
+		assertEquals(true, ah.isUserAdmin("Rahal"));
+	}
+	
+	@Test
+	public void testIsUserAdminFails(){
+		assertEquals(false, ah.isUserAdmin("Rahal1"));
 	}
 	
 	@Test
 	public void testIsUserStudent(){
-		
+		assertEquals(true, ah.isUserStudent("nicoleK"));
+	}
+	
+	@Test
+	public void testIsUserStudentFails(){
+		assertEquals(false, ah.isUserStudent("nicoleK1"));
 	}
 }
